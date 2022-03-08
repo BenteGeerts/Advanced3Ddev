62c8ea074dd5dabed114dc0f4715edadd8508743
BenteG <bente.geerts@student.kdg.be>
2022-03-08 13:27:45 +0100

    added ghost and created EnemySearch script

---
+++ Advanced3Ddev/Assets/Scripts/EnemySearch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySearch : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public Transform seekTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newDirection = seekTarget.position - transform.position;
+        newDirection = new Vector3(newDirection.x, 0, newDirection.z);
+        newDirection = newDirection.normalized;
+        direction = newDirection;
+        transform.position += speed * direction * Time.deltaTime;
+    }
+}
